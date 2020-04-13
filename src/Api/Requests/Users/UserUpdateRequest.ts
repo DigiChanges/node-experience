8a5dcd0a2ae8db703702634d92129485a3377307
Murzbul <natanaelrusso18@hotmail.com>
2020-04-13 19:58:45 -0300

    Fix update user

--- src/Api/Requests/Users/UserUpdateRequest.ts
+++ src/Api/Requests/Users/UserUpdateRequest.ts
@@ -16,16 +16,6 @@ class UserUpdateRequest implements UserUpdatePayload
         return this.request.body.email;
     }
 
-    password(): string
-    {
-        return this.request.body.password;
-    }
-
-    passwordConfirmation(): string
-    {
-        return this.request.body.passwordConfirmation;
-    }
-
     enable(): boolean
     {
         return this.request.body.hasOwnProperty('enable') ? this.request.body.enable : true;
@@ -42,15 +32,12 @@ class UserUpdateRequest implements UserUpdatePayload
             body('email')
                 .exists().withMessage('Email must exist')
                 .isString().withMessage('Name must be of type string'),
-            body('password')
-                .exists().withMessage('Type must exist')
-                .isString().withMessage('Type must be of type integer'),
             body('enable')
                 .optional()
                 .isBoolean().withMessage('Enable must be of type boolean'),
             param('id')
                 .exists().withMessage('ID mus exist')
-                .isUUID().withMessage('Id must UUID type')
+                .isString().withMessage('Id must UUID type')
         ];
     }
 }
--- src/Payloads/Users/UserUpdatePayload.ts
+++ src/Payloads/Users/UserUpdatePayload.ts
@@ -1,8 +1,10 @@
 import IdPayload from "../Defaults/IdPayload";
 import UserRepPayload from "./UserRepPayload";
 
-interface UserUpdatePayload extends IdPayload, UserRepPayload
+interface UserUpdatePayload extends IdPayload
 {
+    email(): string;
+    enable(): boolean;
 }
 
 export default UserUpdatePayload
