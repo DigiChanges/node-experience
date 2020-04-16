039519c31e003a37e10d96fe49c60688b4389a91
Gustavo Garcete <gustavogarcete@gmail.com>
2020-04-16 14:54:45 -0300

    auth middleware, token validation.

---
+++ src/Payloads/Auth/KeepAlivePayload.ts
@@ -0,0 +1,5 @@
+interface KeepAlivePayload {
+    email(): string;
+}
+
+export default KeepAlivePayload
--- src/Services/AuthService.ts
+++ src/Services/AuthService.ts
@@ -7,7 +7,9 @@ import IEncription from "../Lib/Encription/IEncription";
 import {TYPES} from "../types";
 import ErrorException from "../Lib/ErrorException";
 import StatusCode from "../Lib/StatusCode";
-import IToken from "../Lib/Auth/IToken"
+import IToken from "../Lib/Auth/IToken";
+import jwt from "jwt-simple";
+import config from "../../config/config";
 
 @injectable()
 class AuthService
@@ -33,6 +35,22 @@ class AuthService
         {
             throw new ErrorException(StatusCode.HTTP_FORBIDDEN, 'Error credentials');
         }
+        
+        return await this.tokenFactory.token(user);
+    }
+
+    static decodeToken (token: string): any
+    {
+        let TokenArray = token.split(" ");
+
+        let secret = String(config.jwt.secret);
+        
+        return jwt.decode(TokenArray[1], secret);
+    }
+
+    public async regenerateToken (email: string): Promise<IToken>
+    {
+        const user =  await this.repository.getOneByEmail(email);
 
         return await this.tokenFactory.token(user);
     }
