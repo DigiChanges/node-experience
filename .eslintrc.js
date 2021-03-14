f38db42cbfe3f0e6304794e168359f029a922deb
Leonardo Casales <leonardo@smartnet.com.ar>
2021-03-13 22:12:27 -0300

    refactor: quotes single

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,38 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module'
     },
-    "plugins": [
-        "@typescript-eslint"
+    'plugins': [
+        '@typescript-eslint'
     ],
-    "rules": {
+    'rules': {
         indent: [1, 4, {SwitchCase: 1}],
-        quotes: ["warn", "double"],
-        semi: ["warn", "always"],
-        "semi-spacing": ["warn", {"before": false, "after": true}],
-        "comma-spacing": ["warn", {"before": false, "after": true}],
-        "space-infix-ops": 2,
-        "space-in-parens": [1, "never"],
-        "array-bracket-spacing": ["warn", "never"],
-        "object-curly-spacing": ["warn", "never"],
-        "block-spacing": "warn",
-        "arrow-spacing": "warn",
-        "space-before-function-paren": ["warn", "never"],
-        "keyword-spacing": ["warn", {"before": true}],
-        "linebreak-style": ["error", "unix"],
-        "brace-style": ["error", "allman"],
-        "no-unused-vars": ["error", {"args": "none"}],
-        "prefer-const": ["warn"],
+        quotes: ['warn', 'single'],
+        semi: ['warn', 'always'],
+        'semi-spacing': ['warn', {'before': false, 'after': true}],
+        'comma-spacing': ['warn', {'before': false, 'after': true}],
+        'space-infix-ops': 2,
+        'space-in-parens': [1, 'never'],
+        'array-bracket-spacing': ['warn', 'never'],
+        'object-curly-spacing': ['warn', 'never'],
+        'block-spacing': 'warn',
+        'arrow-spacing': 'warn',
+        'space-before-function-paren': ['warn', 'never'],
+        'keyword-spacing': ['warn', {'before': true}],
+        'linebreak-style': ['error', 'unix'],
+        'brace-style': ['error', 'allman'],
+        'no-unused-vars': ['error', {'args': 'none'}],
+        'prefer-const': ['warn'],
     }   
 };
