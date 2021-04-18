1a6b2ffff8bdeccbf1969a836eb9a42f81f6ecde
Natanael Russo <natanaelrusso18@hotmail.com>
2021-04-18 13:41:20 -0300

    fix : Issue #30

--- test.build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export NODE_ENV=test
-
-node dist/src/command.js addUserRole --role Admin --email user@node.com --firstName node --lastName node --password 12345678 --isSuperAdmin false
-node dist/src/command.js addUserRole --role SuperAdmin --email superadmin@node.com --firstName super --lastName admin --password 12345678 --isSuperAdmin true
-
-yarn start:test
