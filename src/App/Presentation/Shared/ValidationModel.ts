046d7d7b8ed8fdd9ae9f2e9d449d875326b0fef3
Roxnny A Barrios C <roxnny.barrios@gmail.com>
2021-05-29 19:16:57 -0400

    feat: a condition was added to show the errors of the validations made with the @ValidateNested () decorator of class-validator to objects nested within the requests

--- src/App/Presentation/Shared/ValidationModel.ts
+++ src/App/Presentation/Shared/ValidationModel.ts
@@ -1,14 +1,22 @@
 import {ValidationError} from 'class-validator';
+import _ from 'lodash';
 
 class ValidationModel
 {
     property: string;
     constraints: any;
+    children: ValidationModel[];
 
     constructor(errors: ValidationError)
     {
         this.property = errors.property;
         this.constraints = errors.constraints;
+
+        if (!_.isEmpty(errors.children))
+        {
+            this.children = [];
+            errors.children.forEach(_children => this.children.push(new ValidationModel(_children)));
+        }
     }
 }
 
