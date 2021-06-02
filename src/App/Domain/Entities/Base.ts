cb236baa7e9213a0772e5f8e8886cd33c4fa6e5b
Roxnny A Barrios C <roxnny.barrios@gmail.com>
2021-06-02 17:47:56 -0400

    feat: add Base entity

---
+++ src/App/Domain/Entities/Base.ts
@@ -0,0 +1,31 @@
+import IBaseDomain from '../../InterfaceAcapters/IBaseDomain';
+import {v4 as uuidv4} from 'uuid';
+
+class Base implements IBaseDomain
+{
+    _id: string;
+    createdAt: Date;
+    updatedAt: Date;
+
+    constructor()
+    {
+        this._id = uuidv4();
+    }
+
+    getId(): string
+    {
+        return this._id;
+    }
+
+    setId(id: string)
+    {
+        this._id = id;
+    }
+
+    clone(): void
+    {
+        this._id = uuidv4();
+    }
+}
+
+export default Base;
