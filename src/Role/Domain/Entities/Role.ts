3dbf4fe6313f0c5a07e5e25e46123ca085abe646
Roxnny A Barrios C <roxnny.barrios@gmail.com>
2021-06-02 17:58:15 -0400

    refactor: implement base entity in role

--- src/Role/Domain/Entities/Role.ts
+++ src/Role/Domain/Entities/Role.ts
@@ -1,25 +1,12 @@
 import IRoleDomain from '../../InterfaceAdapters/IRoleDomain';
-import {v4 as uuidv4} from 'uuid';
+import Base from '../../../App/Domain/Entities/Base';
 
-class Role implements IRoleDomain
+class Role extends Base implements IRoleDomain
 {
-    _id: string;
     name: string;
     slug: string;
     enable: boolean;
     permissions: string[];
-    createdAt: Date;
-    updatedAt: Date;
-
-    constructor()
-    {
-        this._id = uuidv4();
-    }
-
-    getId(): string
-    {
-        return this._id;
-    }
 }
 
 export default Role;
