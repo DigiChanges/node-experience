4184dd29154f81ab12d91320b90caba3ffcc294b
Roxnny A Barrios C <roxnny.barrios@gmail.com>
2021-09-05 22:11:19 -0400

    feat: a transformer with the minimal data of user was added.

---
+++ src/User/Presentation/Transformers/UserMinimalDataTransformer.ts
@@ -0,0 +1,20 @@
+import {Transformer} from '@digichanges/shared-experience';
+
+import IUserDomain from '../../InterfaceAdapters/IUserDomain';
+import IUserMinimalDataTransformer from '../../InterfaceAdapters/IUserMinimalDataTransformer';
+
+class UserMinimalDataTransformer extends Transformer
+{
+    public transform(user: IUserDomain): IUserMinimalDataTransformer
+    {
+        return {
+            id: user.getId(),
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email,
+            phone: user.phone
+        };
+    }
+}
+
+export default UserMinimalDataTransformer;
