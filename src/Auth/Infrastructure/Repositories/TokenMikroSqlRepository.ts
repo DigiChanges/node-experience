4db1a62be63fc1df9b5ec34ac56a44d83e85b14f
Nathan Russo <nrusso@digichanes.com>
2022-01-19 19:04:48 -0300

    fix: typo token mikro

--- src/Auth/Infrastructure/Repositories/TokenMikroSqlRepository.ts
+++ src/Auth/Infrastructure/Repositories/TokenMikroSqlRepository.ts
@@ -3,7 +3,7 @@ import { injectable } from 'inversify';
 import BaseMikroSqlRepository from '../../../App/Infrastructure/Repositories/BaseMikroSqlRepository';
 import Token from '../../Domain/Entities/Token';
 import ITokenDomain from '../../InterfaceAdapters/ITokenDomain';
-import TokenSchema from '../Schemas/TokenTypeORM';
+import TokenSchema from '../Schemas/TokenMikroORM';
 
 @injectable()
 class TokenMikroSqlRepository extends BaseMikroSqlRepository<ITokenDomain> implements ITokenRepository<ITokenDomain>
