2c57c160884a7545e86732bea2c5ba5b3cb0a276
Leonardo Casales <leonardo@smartnet.com.ar>
2022-06-03 22:24:46 -0300

    fix: decorator inject containers

--- src/Shared/Decorators/ContainerFactory.ts
+++ src/Shared/Decorators/ContainerFactory.ts
@@ -1,16 +1,21 @@
 import container from '../../inversify.config';
-import { SERVICES_IDENTIFIER } from '../../serviceIdentifier';
-import InjectionFactory from '../Factories/InjectionFactory';
+import getDecorators from 'inversify-inject-decorators';
 
-export function containerFactory(serviceIdentifier: SERVICES_IDENTIFIER, doCache = true)
+// setup the container...
+const { lazyInject: originalLazyInject } = getDecorators(container);
+
+// Additional function to make properties decorators compatible with babel.
+function fixPropertyDecorator<T extends Function>(decorator: T): T
 {
-    return (proto: any, key: string): void =>
+    return ((...args: any[]) => (
+        target: any,
+        propertyName: any,
+        ...decoratorArgs: any[]
+    ) =>
     {
-        const resolve = () =>
-        {
-            return container.get(serviceIdentifier);
-        };
-
-        InjectionFactory._proxyGetter(proto, key, resolve, doCache);
-    };
+        decorator(...args)(target, propertyName, ...decoratorArgs);
+        return Object.getOwnPropertyDescriptor(target, propertyName);
+    }) as any;
 }
+
+export const containerFactory = fixPropertyDecorator(originalLazyInject);
