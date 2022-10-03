74d36522d92d0aeb2a953ba0bfef2541c9dcb35a
segoviadanieldev <segoviadanieldev@gmail.com>
2022-10-03 10:41:00 -0300

    feat: add test to mikroORM

--- src/Shared/Infrastructure/Database/CreateMikroORMConnection.ts
+++ src/Shared/Infrastructure/Database/CreateMikroORMConnection.ts
@@ -1,10 +1,12 @@
 import { MikroORM } from '@mikro-orm/core';
+import { newDb } from 'pg-mem';
 import User from '../../../User/Infrastructure/Schemas/UserMikroORM';
 import Role from '../../../Role/Infrastructure/Schemas/RoleMikroORM';
 import Item from '../../../Item/Infrastructure/Schemas/ItemMikroORM';
 import File from '../../../File/Infrastructure/Schemas/FileMikroORM';
 import ICreateConnection from './ICreateConnection';
 import Logger from '../../Application/Logger/Logger';
+import { DataSource } from 'typeorm';
 // import Notification from '../../Notification/Infrastructure/Schemas/NotificationMikroORM';
 // import TokenSchema from '../../AuthHelper/Infrastructure/Schemas/TokenMikroORM';
 
@@ -45,9 +47,69 @@ class CreateMikroORMConnection implements ICreateConnection
         };
     }
 
-    async initConfigTest(): Promise<any>
+    async initConfigTest(): Promise<void>
     {
-        return Promise.resolve(undefined); // TODO: Set init config
+        const db = newDb({
+            autoCreateForeignKeyIndices: true
+        });
+
+        db.public.registerFunction({
+            implementation: () => 'experience',
+            name: 'current_database'
+        });
+
+        db.public.registerFunction({
+            name: 'version',
+            implementation: () =>
+            {
+                return 'PostgreSQL 11.16 (Debian 11.16-1.pgdg90+1) on x86_64-pc-linux-gnu, compiled by gcc (Debian 6.3.0-18+deb9u1) 6.3.0 20170516, 64-bit';
+            }
+        });
+
+        db.public.registerFunction({
+            name: 'pg_views',
+            implementation: () =>
+            {
+                return [];
+            }
+        });
+
+        db.public.registerFunction({
+            name: 'pg_matviews',
+            implementation: () =>
+            {
+                return null;
+            }
+        });
+
+        db.public.interceptQueries(text =>
+        {
+            if (text === 'SELECT \'DROP VIEW IF EXISTS "\' || schemaname || \'"."\' || viewname || \'" CASCADE;\' as "query" FROM "pg_views" WHERE "schemaname" IN (current_schema()) AND "viewname" NOT IN (\'geography_columns\', \'geometry_columns\', \'raster_columns\', \'raster_overviews\')')
+            {
+                return [];
+            }
+
+            if (text === 'SELECT \'DROP MATERIALIZED VIEW IF EXISTS "\' || schemaname || \'"."\' || matviewname || \'" CASCADE;\' as "query" FROM "pg_matviews" WHERE "schemaname" IN (current_schema())')
+            {
+                return [];
+            }
+
+            if (text === 'SELECT \'DROP TABLE IF EXISTS "\' || schemaname || \'"."\' || tablename || \'" CASCADE;\' as "query" FROM "pg_tables" WHERE "schemaname" IN (current_schema()) AND "tablename" NOT IN (\'spatial_ref_sys\')')
+            {
+                return [];
+            }
+
+            if (text === 'SELECT \'DROP TYPE IF EXISTS "\' || n.nspname || \'"."\' || t.typname || \'" CASCADE;\' as "query" FROM "pg_type" "t" INNER JOIN "pg_enum" "e" ON "e"."enumtypid" = "t"."oid" INNER JOIN "pg_namespace" "n" ON "n"."oid" = "t"."typnamespace" WHERE "n"."nspname" IN (current_schema()) GROUP BY "n"."nspname", "t"."typname"')
+            {
+                return [];
+            }
+
+            return null;
+        });
+
+        orm = await db.adapters.createMikroOrm({
+            entities: [...this.entities]
+        });
     }
 
     async create(): Promise<any>
