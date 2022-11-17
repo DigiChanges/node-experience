1d2dc14da46de14bd06d1e7a0d2a50ac82bf1528
nrusso <nrusso@digichanges.com>
2022-11-17 13:21:10 -0300

    feat: improve esbuild compiler and add last eslint version

--- etsc.config.js
+++ etsc.config.js
@@ -1,15 +1,24 @@
-const esbuildPluginTsc = require("esbuild-plugin-tsc");
-
 module.exports = {
-  outDir: "./dist",
   esbuild: {
     minify: false,
-    target: "es2019",
-    plugins: [esbuildPluginTsc()],
+    target: "es2020",
   },
-  assets: {
-    baseDir: "src",
-    outDir: "./dist/src",
-    filePatterns: ["**/*.json", "**/*.hbs"],
+  prebuild: async () => {
+    console.log("prebuild");
+    const rimraf = (await import("rimraf")).default;
+    rimraf.sync("./dist"); // clean up dist folder
+  },
+  postbuild: async () => {
+    console.log("postbuild");
+    const cpy = (await import("cpy")).default;
+    await cpy(
+      [
+        "src/**/*.graphql", // Copy all .graphql files
+        "!src/**/*.{tsx,ts,js,jsx}", // Ignore already built files
+        "src/**/*.json",
+        "src/**/*.hbs"
+      ],
+      "dist/src"
+    );
   },
 };
