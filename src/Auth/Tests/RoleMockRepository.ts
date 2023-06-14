be6dce946e996cfea6310be81b5aa050b6d5be49
KMery <kmeryleiva@gmail.com>
2023-06-14 12:49:28 -0300

    feat: nse-149 mock data shared setted in separate file

--- src/Auth/Tests/RoleMockRepository.ts
+++ src/Auth/Tests/RoleMockRepository.ts
@@ -1,62 +1,7 @@
 import ICriteria from '../../Shared/Presentation/Requests/ICriteria';
 import IPaginator from '../../Shared/Infrastructure/Orm/IPaginator';
 import IRoleDomain from '../Domain/Entities/IRoleDomain';
-
-const responseIPaginator: IPaginator =
-{
-    getCurrentPage(): number
-    {
-        return 0;
-    }, getCurrentUrl(): string
-    {
-        return '';
-    }, getExist(): boolean
-    {
-        return false;
-    }, getFirstUrl(): string
-    {
-        return '';
-    }, getFrom(): number
-    {
-        return 0;
-    }, getLasPage(): number
-    {
-        return 0;
-    }, getLastUrl(): string
-    {
-        return '';
-    }, getLimit(): number
-    {
-        return 0;
-    }, getMetadata(): Record<string, unknown>
-    {
-        return undefined;
-    }, getNextUrl(): string | null
-    {
-        return undefined;
-    }, getOffset(): number
-    {
-        return 0;
-    }, getPath(): string
-    {
-        return '';
-    }, getPerPage(): number
-    {
-        return 0;
-    }, getPrevUrl(): string | null
-    {
-        return undefined;
-    }, getTo(): number
-    {
-        return 0;
-    }, getTotal(): number
-    {
-        return 0;
-    }, paginate(): Promise<unknown>
-    {
-        return Promise.resolve(undefined);
-    }
-};
+import { responseIPaginator } from './DataMock';
 
 const responseIRoleDomain = {
     createdAt: undefined, name: '', permissions: [], updatedAt: undefined
