d604b9754318a05ddd700891373383288be52943
nrusso <natanaelrusso18@hotmail.com>
2023-06-22 13:39:42 -0300

    fix: test

--- src/File/Presentation/Routes/FileKoaRouter.ts
+++ src/File/Presentation/Routes/FileKoaRouter.ts
@@ -2,6 +2,7 @@ import Router from 'koa-router';
 import FileController from '../Controllers/FileKoaController';
 import AuthorizeKoaMiddleware from '../../../Auth/Presentation/Middlewares/AuthorizeKoaMiddleware';
 import Permissions from '../../../Config/Permissions';
+import FileKoaReqMulterMiddleware from '../Middlewares/FileKoaReqMulterMiddleware';
 
 const routerOpts: Router.IRouterOptions = {
     prefix: '/api/files'
@@ -13,7 +14,7 @@ FileKoaRouter.get('/', AuthorizeKoaMiddleware(Permissions.FILES_LIST), FileContr
 FileKoaRouter.get('/objects', AuthorizeKoaMiddleware(Permissions.FILES_LIST), FileController.listObjects);
 FileKoaRouter.get('/metadata/:id', AuthorizeKoaMiddleware(Permissions.FILES_SHOW_METADATA), FileController.getFileMetadata);
 FileKoaRouter.post('/base64', AuthorizeKoaMiddleware(Permissions.FILES_UPLOAD), FileController.uploadBase64);
-FileKoaRouter.post('/', AuthorizeKoaMiddleware(Permissions.FILES_UPLOAD), FileController.uploadMultipart);
+FileKoaRouter.post('/', <any>FileKoaReqMulterMiddleware.single('file'), AuthorizeKoaMiddleware(Permissions.FILES_UPLOAD), FileController.uploadMultipart);
 FileKoaRouter.post('/presigned-get-object', AuthorizeKoaMiddleware(Permissions.FILES_LIST), FileController.getPresignedGetObject);
 FileKoaRouter.get('/:id', AuthorizeKoaMiddleware(Permissions.FILES_DOWNLOAD), FileController.download);
 FileKoaRouter.put('/optimize/:id', AuthorizeKoaMiddleware(Permissions.FILES_UPDATE), FileController.optimize);
