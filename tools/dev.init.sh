23ab0552fd35262f9a41f0d52904085b7c06c81a
ngrosso <nzgrosso@gmail.com>
2023-08-30 20:19:48 -0300

    fix: gender attribute

--- tools/dev.init.sh
+++ tools/dev.init.sh
@@ -2,6 +2,6 @@
 
 pnpm command syncRolesPermission
 pnpm sync:db
-pnpm command addUser --role Admin --email admin@node.com --firstName super --lastName admin --genre M --phone 541112345678 --country AR --password 12345678 --birthdate 1990-07-01
-pnpm command addUser --role Operator --email user@node.com --firstName node --lastName node --genre M --phone 541112345678 --country AR --password 12345678 --birthdate 1990-07-20
+pnpm command addUser --role Admin --email admin@node.com --firstName super --lastName admin --gender M --phone 541112345678 --country AR --password 12345678 --birthdate 1990-07-01
+pnpm command addUser --role Operator --email user@node.com --firstName node --lastName node --gender M --phone 541112345678 --country AR --password 12345678 --birthdate 1990-07-20
 pnpm command createBucket --name experience --region us-east-1
