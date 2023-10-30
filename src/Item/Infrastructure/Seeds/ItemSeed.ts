42d8198350b13dfc03cb9137d3ca4fb5e69e69e3
nrusso <nrusso@igichanges.com>
2023-10-30 09:12:50 -0300

    refactor(Item): replace save use case in item seed

--- src/Item/Infrastructure/Seeds/ItemSeed.ts
+++ src/Item/Infrastructure/Seeds/ItemSeed.ts
@@ -1,36 +1,26 @@
 import { faker } from '@faker-js/faker';
-import IItemRepository from '../Repositories/IItemRepository';
-import { REPOSITORIES } from '../../../Config/Injects';
 import BaseSeed from '../../../Shared/Infrastructure/Seeds/BaseSeed';
-import IItemDomain from '../../Domain/Entities/IItemDomain';
-import ItemBuilder from '../../Domain/Factories/ItemBuilder';
 import ISeed from '../../../Shared/Infrastructure/Seeds/ISeed';
+import SaveItemUseCase from '../../Domain/UseCases/SaveItemUseCase';
 
 class ItemSeed extends BaseSeed implements ISeed
 {
-    private repository: IItemRepository;
-
     constructor()
     {
         super();
-        this.repository = this.container.resolve<IItemRepository>(REPOSITORIES.IItemRepository);
     }
 
     public async init()
     {
-        const indexes = Array.from({ length: 10 }, (v, i) => i + 1);
+        const indexes = Array.from({ length: 10 }, (_, i) => i + 1);
 
-        for await (const index of indexes)
+        for await (const _ of indexes)
         {
             const name = faker.person.firstName();
             const type = faker.number.int({ max: 100 });
 
-            const item: IItemDomain = new ItemBuilder({ name, type })
-                .setItem()
-                .build()
-                .create();
-
-            await this.repository.save(item);
+            const saveItemUseCase = new SaveItemUseCase();
+            await saveItemUseCase.handle({ name, type });
         }
     }
 }
